3ff940bf5b3453764e9f93150f393c063cd07381
Ivo Schwizer <ivoschwizer@gmail.com>
2022-04-04 13:19:33 +0200

    projekt

---
+++ projekt-ueberbauung-shield-shelter-amriswil-thurgau.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if IE 7]>	 <html class="no-js lt-ie9 lt-ie8" lang="de"><![endif]-->
+<!--[if IE 8]>	 <html class="no-js lt-ie9" lang="de"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="de">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>Art of Loft - Projekt - Überbauung Shield &amp; Shelter - Amriswil Thurgau</title>
+    <meta name="description" content="Art of Loft - Projekt - Überbauung Shield &amp; Shelter - Amriswil Thurgau">
+    <?php include 'head.php'; ?>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+  <div class="maincontainer">
+    <div class="logo">
+  		<a href="index"><img src="img/artofloft-logo.svg" alt="Art of Loft Logo" width="220px" height="auto"></a>
+  	</div>
+    <!--container-projekt-->
+    <section class="projektcontainer">
+      <div class="description">
+        <h1>Überbauung "Shield & Shelter" (2009) | Amriswil TG</span><span>Neubau von Büro- und Geschäftshaus Promega AG</span><span>Projektstatus:  Realisiert </span></h1>
+      </div>
+      <figure class="projektstartimg">
+        <img src="img/art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-01.jpg" alt="art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-01">
+      </figure>
+      <figure class="projektimg high">
+        <img src="img/art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-02.jpg" alt="art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-02">
+      </figure>
+      <figure class="projektimg high middle">
+        <img src="img/art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-03.jpg" alt="art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-03">
+      </figure>
+      <figure class="projektimg high">
+        <img src="img/art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-04.jpg" alt="art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-04">
+      </figure>
+      <figure class="projektimg fullmiddle">
+        <img src="img/art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-05.jpg" alt="art-of-loft-projekt-ueberbauung-shield-shelter-amriswil-thurgau-05">
+      </figure>
+    </section>
+
+
+    <section class="projektallcontainer">
+      <h1 class="title sticky"> <span>Alle</span> Projekte</h1>
+      <?php include 'projekt-overall.php'; ?>
+      <style> #object-Ap02 { display: none; } </style>
+
+    </section>
+
+  </div>
+
+
+  <?php include 'footer.php'; ?>
+  <?php include 'script.php'; ?>
+</body>
+</html>

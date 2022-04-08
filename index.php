7e32aa3f90c6c742c3b9070f51c079906c523896
Ivo Schwizer <ivoschwizer@gmail.com>
2022-04-08 16:09:53 +0200

    de

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
   <?php include 'header.php'; ?>
 
-  <div class="maincontainer respfix">
+  <div class="maincontainer">
     <div class="logo">
       <a href="index"><img src="img/artofloft-logo.svg" alt="Art of Loft Logo" width="220px" height="auto"></a>
     </div>
